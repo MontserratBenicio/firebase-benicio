0ec99cc04f955f01189fd8c2df8629a1f9153a11
MontserratBenicio <93968890+MontserratBenicio@users.noreply.github.com>
2021-11-09 01:02:31 -0600

    Create init.js

---
+++ init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+ apiKey: "AIzaSyB3lXBYkH8_oN-TQ5Namunf2XIp0YUYdIE",
+  authDomain: "mbm-proyecto.firebaseapp.com",
+  projectId: "mbm-proyecto",
+  storageBucket: "mbm-proyecto.appspot.com",
+  messagingSenderId: "99478322574",
+  appId: "1:99478322574:web:e8ce78b1d4dbb05250f8df",
+  measurementId: "G-71J10T2GT8"
+});
